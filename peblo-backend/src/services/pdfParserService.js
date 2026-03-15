e52908e65cafbc6dd7ac0c4b6147d79949eb46e4
ShivaChary1 <shivachary.ul@gmail.com>
2026-03-15 16:50:04 +0530

    first commit

---
+++ peblo-backend/src/services/pdfParserService.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const pdfParse = require("pdf-parse");
+
+const extractTextFromPDF = async (filePath) => {
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+
+    const pdfData = await pdfParse(dataBuffer);
+
+    return pdfData.text;
+  } catch (error) {
+    console.error("PDF parsing error:", error);
+    throw error;
+  }
+};
+
+module.exports = {
+  extractTextFromPDF
+};
